5c49deca0a0d0950352ae57167405411262fd388
Estiven Giraldo Acevedo <estivengiraldo123@hotmail.com>
2022-10-24 18:09:14 -0500

    clase1 carro compras

---
+++ CarroCompras/CarroCompras/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarroCompras.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
